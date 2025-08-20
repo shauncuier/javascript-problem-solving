3ca7a48dc2ccdede82850c6e3cdca1d0e45257c6
Jashedul Islam Shaun <shauncuier@gmail.com>
2025-08-20 18:32:43 +0600

    Add JavaScript functions for various problems in problems.js

---
+++ problems.js
@@ -0,0 +1,100 @@
+// Problem 1: Reverse a String
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+
+
+
+
+// Problem 2: Count Vowels in a String
+function countVowels(str) {
+    let vowels = "aeiouAEIOU";
+    let count = 0;
+    for (let char of str) {
+        if (vowels.includes(char)) {
+            count++;
+        }
+    }
+    return count;
+}
+
+// Problem 3: Check for Palindrome
+function isPalindrome(str) {
+    let reversed = str.split("").reverse().join("");
+    return str === reversed;
+}
+
+// Problem 4: Find the Maximum Number
+function findMax(arr) {
+    let max = arr[0];
+    for (let num of arr) {
+        if (num > max) {
+            max = num;
+        }
+    }
+    return max;
+}
+
+// Problem 5: Remove Duplicates from an Array
+function removeDuplicates(arr) {
+    let unique = [];
+    for (let num of arr) {
+        if (!unique.includes(num)) {
+            unique.push(num);
+        }
+    }
+    return unique;
+}
+
+// Problem 6: Sum of All Numbers in an Array
+function sumArray(arr) {
+    let sum = 0;
+    for (let num of arr) {
+        sum += num;
+    }
+    return sum;
+}
+
+// Problem 7: Find Even Numbers in an Array
+function findEvens(arr) {
+    let evens = [];
+    for (let num of arr) {
+        if (num % 2 === 0) {
+            evens.push(num);
+        }
+    }
+    return evens;
+}
+
+// Problem 8: Capitalize First Letter of Each Word
+function capitalizeWords(str) {
+    let words = str.split(" ");
+    for (let i = 0; i < words.length; i++) {
+        words[i] = words[i][0].toUpperCase() + words[i].slice(1);
+    }
+    return words.join(" ");
+}
+
+// Problem 9: Find the Factorial of a Number
+function factorial(n) {
+    let result = 1;
+    for (let i = 1; i <= n; i++) {
+        result *= i;
+    }
+    return result;
+}
+
+// Problem 10: PingPong Challenge
+function pingPong() {
+    for (let i = 1; i <= 20; i++) {
+        if (i % 3 === 0 && i % 5 === 0) {
+            console.log("PingPong");
+        } else if (i % 3 === 0) {
+            console.log("Ping");
+        } else if (i % 5 === 0) {
+            console.log("Pong");
+        } else {
+            console.log(i);
+        }
+    }
+}
